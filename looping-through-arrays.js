6bf219699e6a8db6b83216e27dca4037528f6d1c
AngellaBaker <angellambaker@gmail.com>
2016-08-29 14:40:28 -0500

    adding challenge 14 of javascripting -looping through arrays

---
+++ looping-through-arrays.js
@@ -0,0 +1,8 @@
+// define pets array
+var pets = ['cat', 'dog', 'rat'];
+// loop to append s to each string in array pets
+for (var i in pets) {
+  pets[i] = pets[i] + 's';
+}
+// print pets to console
+console.log(pets);
